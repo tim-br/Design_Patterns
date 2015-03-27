87bee4688958ce490f8d6625ff75dbdea78b3494
timothy <t.williams.im@gmail.com>
2015-03-27 02:24:57 -0400

    add a class demo .js file

---
+++ Javascript/car_class_demo.js
@@ -0,0 +1,34 @@
+function Car(model){
+  this.model = model;
+  this.color = 'silver';
+  this.year = '2012';
+  this.getInfo = function(){
+    return this.model + ' ' + this.year;
+  }
+}
+
+var myCar = new Car('ford');
+myCar.year = '2010';
+console.log(myCar.getInfo());
+
+var myOtherCar = {
+  model: "mustang",
+  year: '1999',
+  getInfo: function () {
+    return this.model + ' ' + this.year;
+  }
+}
+
+console.log(myOtherCar.getInfo());
+//already in the instance, all of these are singletons
+
+var myThirdCar = new function(){
+  this.model = 'honda';
+  this.color = 'silver';
+  this.year = '1985';
+  this.getInfo = function(){
+    return this.model + ' ' + this.year;
+  }
+}
+
+console.log(myThirdCar.getInfo());
